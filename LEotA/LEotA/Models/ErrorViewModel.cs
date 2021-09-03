1eee94e627fe0042834809555e6407b88314e56d
fizzlefrost <37946205+fizzlefrost@users.noreply.github.com>
2021-09-03 21:18:42 +0900

    Initial commit

---
+++ LEotA/LEotA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LEotA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
