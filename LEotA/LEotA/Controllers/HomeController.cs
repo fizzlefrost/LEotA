4333c6dd7c7fed1fa77250fae0263e33c4040a80
fizzlefrost <englingurru@gmail.com>
2021-10-06 13:45:28 +0900

    Revert "test change"
    This reverts commit b9f4bcf76b7f1d938226ecc251ba439d2aa53ead.

--- LEotA/LEotA/Controllers/HomeController.cs
+++ LEotA/LEotA/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace LEotA.Controllers
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
