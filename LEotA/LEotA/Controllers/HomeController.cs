9d6b78d1b7ef6b9d22b20d22fad27aaf20f1962d
fizzlefrost <englingurru@gmail.com>
2021-10-11 21:08:46 +0900

    Fixed navbar/test change of css/updated views

--- LEotA/LEotA/Controllers/HomeController.cs
+++ LEotA/LEotA/Controllers/HomeController.cs
@@ -23,7 +23,27 @@ namespace LEotA.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Staff()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Publications()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+        public IActionResult Events()
         {
             return View();
         }
@@ -39,126 +59,6 @@ namespace LEotA.Controllers
         private readonly ILogger<MaterialsController> _logger;
 
         public MaterialsController(ILogger<MaterialsController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
-    }
-    public class EventsController : Controller
-    {
-        private readonly ILogger<EventsController> _logger;
-
-        public EventsController(ILogger<EventsController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
-    }
-    public class GalleryController : Controller
-    {
-        private readonly ILogger<GalleryController> _logger;
-
-        public GalleryController(ILogger<GalleryController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
-    }
-    public class StaffController : Controller
-    {
-        private readonly ILogger<StaffController> _logger;
-
-        public StaffController(ILogger<StaffController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
-    }
-    public class AboutUsController : Controller
-    {
-        private readonly ILogger<AboutUsController> _logger;
-
-        public AboutUsController(ILogger<AboutUsController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
-    }
-    public class ProjectsController : Controller
-    {
-        private readonly ILogger<ProjectsController> _logger;
-
-        public ProjectsController(ILogger<ProjectsController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
-    }
-    public class PublicationsController : Controller
-    {
-        private readonly ILogger<PublicationsController> _logger;
-
-        public PublicationsController(ILogger<PublicationsController> logger)
         {
             _logger = logger;
         }
