c6393c78e72a6deae12414d0772087d61fd82959
Леонид Имеев <leonidimeev@yandex.ru>
2021-10-17 14:04:00 +0900

    GetClientSecretTestPage initialization

---
+++ LEotA.Ng/Pages/GetClientSecretTestPage.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LEotA.Pages
+{
+    public class GetClientSecretTestPage : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["Message"] = "asdasd";
+        }
+    }
+}
