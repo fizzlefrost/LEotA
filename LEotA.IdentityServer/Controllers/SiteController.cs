36f3dced314de44f777cf7ecdb0c2e39ef621de9
Леонид Имеев <leonidimeev@yandex.ru>
2021-10-17 14:03:37 +0900

    IdentityServer initialization

---
+++ LEotA.IdentityServer/Controllers/SiteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LEotA.IdentityServer.Controllers
+{
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
