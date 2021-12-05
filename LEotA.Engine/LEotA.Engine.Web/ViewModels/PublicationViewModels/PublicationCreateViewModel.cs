004cee85c56eea3580a8fad9e78d6ec59625aca4
Леонид Имеев <leonidimeev@yandex.ru>
2021-12-05 22:16:42 +0900

    PublicationCreateViewModel initialization

---
+++ LEotA.Engine/LEotA.Engine.Web/ViewModels/PublicationViewModels/PublicationCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Calabonga.EntityFrameworkCore.Entities.Base;
+
+namespace LEotA.Engine.Web.ViewModels.PublicationViewModels
+{
+    public class PublicationCreateViewModel : IViewModel
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string EmbedLink { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
