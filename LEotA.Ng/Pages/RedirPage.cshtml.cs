0e755f98622e55f832395bd42e82e5348c8884b3
fizzlefrost <englingurru@gmail.com>
2022-01-21 19:32:14 +0900

    redir page

---
+++ LEotA.Ng/Pages/RedirPage.cshtml.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LEotA.Pages
+{
+    public class RedirPageModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+        
+    }
+}
