6b0e40311312c17f8978fb63c00d129b58eb346d
fizzlefrost <englingurru@gmail.com>
2022-02-03 01:45:20 +0900

    different foreach??????????? removed sort

--- LEotA.Ng/Pages/Staff.cshtml.cs
+++ LEotA.Ng/Pages/Staff.cshtml.cs
@@ -27,10 +27,9 @@ namespace LEotA.Pages
         {
             //try
             //{
-                var staffList = _engineClientManager.StaffGetPaged(null,30,null,null,true);
-                var sortedStaffList = staffList.OrderBy(o => o.Role).ToList();
+                var staffList = _engineClientManager.StaffGetPaged(null,50,null,null,true);
                 var staffListWithImage = new Dictionary<Staff, List<FileContent>>();
-                foreach (var staff in sortedStaffList)
+                foreach (var staff in staffList)
                 {
                     var image = _engineClientManager.FileContentGetByMasterId(staff.Id);
                 
