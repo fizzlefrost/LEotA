9ac95c1bdea459e56ffbc98a7e9ee41fa923b332
fizzlefrost <englingurru@gmail.com>
2022-02-10 01:09:07 +0900

    news image added

--- LEotA.Ng/Pages/News.cshtml.cs
+++ LEotA.Ng/Pages/News.cshtml.cs
@@ -1,8 +1,11 @@
 ﻿#nullable enable
 using System;
+using System.Collections.Generic;
 using LEotA.Clients.EngineClient;
+using LEotA.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LEotA.Pages
 {
@@ -16,8 +19,15 @@ namespace LEotA.Pages
         }
         public IActionResult OnGet(Guid id)
         {
+            var newsWithImage = new Dictionary<News, List<FileContent>?>();
             ViewData.Clear();
             var news = _engineClientManager.NewsGetById(id)?.Result;
+            if (news != null)
+            {
+                var newsImage = _engineClientManager.FileContentGetByMasterId(news.Id);
+                newsWithImage.Add(news, newsImage);
+                ViewData.Add("newsId",newsWithImage);
+            }
             ViewData.Add("newsId", news);
             return Page(); 
         }
