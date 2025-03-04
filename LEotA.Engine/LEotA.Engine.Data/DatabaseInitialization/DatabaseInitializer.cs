250c62c254cf2cd7fc155c026d7f021fe604736a
Леонид Имеев <leonidimeev@yandex.ru>
2025-03-04 13:27:04 +0900

    Update DatabaseInitializer.cs

--- LEotA.Engine/LEotA.Engine.Data/DatabaseInitialization/DatabaseInitializer.cs
+++ LEotA.Engine/LEotA.Engine.Data/DatabaseInitialization/DatabaseInitializer.cs
@@ -24,7 +24,7 @@ namespace LEotA.Engine.Data.DatabaseInitialization
 
             // Should be uncomment when using UseSqlServer() settings or any other provider.
             // This is should not be used when UseInMemoryDatabase()
-            await context.Database.MigrateAsync();
+            // await context.Database.MigrateAsync();
 
             var roles = AppData.Roles.ToArray();
 
